ea52564aeaae25a68fd9d0d8faccc7f1e2b974cf
Steren <steren.giannini@gmail.com>
2014-05-08 12:52:23 +0200

    Store user info in NDB model

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 """`main` is the top level module for your Flask application."""
 
-# Import the Flask Framework
+# Import the Flask Framework and modules
 from flask import Flask
 from flask.ext.dropbox import Dropbox, DropboxBlueprint
 from flask import url_for, redirect, request
+from flask import g, request, session as flask_session, url_for
+
+# Google NDB
+from google.appengine.ext import ndb
+
+import datetime
 
 # our app settings are stored here.
 import settings
@@ -24,10 +30,58 @@ app.config.from_object(settings)
 dropbox = Dropbox(app)
 dropbox.register_blueprint(url_prefix='/dropbox')
 
+# Models
+class User(ndb.Model):
+  """Models an individual User entry with dropbox credentials and sketchfab API key."""
+  dropbox_uid = ndb.IntegerProperty()
+  dropbox_email = ndb.StringProperty()
+  dropbox_token = ndb.StringProperty()
+  dropbox_cursor = ndb.StringProperty()
+  created_date = ndb.DateTimeProperty(auto_now_add=True)
+  last_login_date = ndb.DateTimeProperty()
+  last_check_date = ndb.DateTimeProperty()
+
+class Upload(ndb.Model):
+  """Models an individual Uploqd entry."""
+  sketchfab_model_id = ndb.StringProperty()
+  dropbox_path = ndb.StringProperty()
+  createdDate = ndb.DateTimeProperty(auto_now_add=True)
+  updatedDate = ndb.DateTimeProperty()
+
+# routes
+
 @app.route('/')
 def home():
+    # return render_template('home.html', authenticated=dropbox.is_authenticated)
     return u'Click <a href="%s">here</a> to login with Dropbox.' % \
            dropbox.login_url
+
+@app.route('/welcome')
+def welcome():
+    # save user info
+    token, secret = flask_session['dropbox_access_token']
+    uid = dropbox.account_info['uid']
+
+    user = User.query(User.dropbox_uid == uid).get()
+    if user:
+        logger.info('Got returning user')
+        user.populate(dropbox_uid=dropbox.account_info['uid'],
+                    dropbox_email = dropbox.account_info['email'],
+                    dropbox_token = token,
+                    last_login_date = datetime.datetime.now())
+    else:
+        logger.info('New user')
+        user = User(dropbox_uid=dropbox.account_info['uid'],
+                    dropbox_email = dropbox.account_info['email'],
+                    dropbox_token = token,
+                    last_login_date = datetime.datetime.now())
+    user.put()
+
+    return u'<form action="" method="post">' \
+           u'<input name="sketchfabapi" type="text">' \
+           u'<input type="submit" value="Send">' \
+           u'</form>'
+
 
 @app.route('/success/<path:filename>')
 def success(filename):
